44e5d06240d2f64245b03e9e2d1e762d44042740
Andrew Perotti <Perotti28@gmail.com>
2025-09-10 10:28:50 -0700

    feat: add state to Education component
    This state will be used to keep status on if the user is editing their education or have submitted their education.
    
    styles: import style file to be used
    
    feat: add conditional return of component
    
    The condition resets on the status state variables value and depending on if it is 'edit' or 'submit' the user will be rendered input fields or saved data in a presentable fashion, which I have not finished but have left comments to remind myself next. This also includes using another component for the input which I've named EducationInput

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,10 +1,39 @@
-// import { useState } from 'react'
-import "../styles/index.css";
+import { useState } from "react";
+import "../styles/Education.css";
+import { EducationInput } from "./EducationInput";
 
 export default function Education() {
-  return (
-    <>
-      <h1>Education:</h1>
-    </>
-  );
+  const [status, setStatus] = useState("edit");
+
+  if (status === "edit") {
+    //if person.education.length===0 render one instance with no values
+    //else if person.education.length>0 render an EducationInput instance for each object in that array
+    return (
+      <div id="education-input">
+        <h1>Education:</h1>
+        <EducationInput />
+        <button
+          id="add-education-component"
+          //add onClick method to generate another Educationinput component, guessing it would add object to person.education array with empty values
+        >
+          Add Another
+        </button>
+        <button onClick={() => setStatus("submit")}>Submit</button>
+        {/* maybe check for inputs when submit is clicked so handfuls of empty
+        education objects aren't added? */}
+      </div>
+    );
+  } else {
+    return (
+      <div id="education-output">
+        {/* //if education.person.length>0: */}
+        <h1>Education:</h1>
+        {/* //generate output for each object in array */}
+        {/* //name of school/course  */}
+        {/* //title of degree/course taken  */}
+        {/* //dates attended/studied */}
+        <button onClick={() => setStatus("edit")}>Edit</button>
+      </div>
+    );
+  }
 }

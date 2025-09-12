88cd01b1b4b1e37dd2c0adf12c66cd237067d4a0
Andrew Perotti <Perotti28@gmail.com>
2025-09-12 12:33:19 -0700

    feat: add validityChecker function and usage
    This function will be invoked whenever there is entry in a required input field at which point it will check if that input is valid against any input attribute types or patterns, and if not valid will apply styling and insert an error message below the input element until the entry is valid.
    
    fix: change asterisk to required fields
    
    Prior to this change the asterisk was only on optional fields, which didn't make sense so I switched its usage.
    
    feat: add required attribute to select inputs
    
    Some inputs are required and pattern attribute was also added to help validate user entry data.
    
    fix: change disclaimer to placeholder text for skills input
    
    feat: add validity check of Submit button
    
    This button when clicked prior to these changes would change the status state variable, but now it checks if the required input elements entry is valid before it makes that change. If not valid it displays an error message to the user and applies error highlighting to the outline of those inputs.

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,64 +1,122 @@
-import { useState } from "react";
+import { Children, createElement, useState } from "react";
 import "../styles/General.css";
 
 export default function General({ person, setPerson }) {
   const [status, setStatus] = useState("edit");
+
+  function validityChecker(
+    event,
+    inputElement,
+    property,
+    errorMessage,
+    errorMessageId,
+  ) {
+    //save input
+    let newPerson = { ...person };
+    newPerson[property] = event.target.value;
+    setPerson({ ...newPerson });
+    //check if specific error message is active, if so delete
+    if (document.querySelector(`#${errorMessageId}`)) {
+      document.querySelector(`#${errorMessageId}`).remove();
+    }
+    //check if input is valid, remove styling
+    if (inputElement.checkValidity() === true) {
+      inputElement.style.outline = "none";
+    } else {
+      inputElement.style.outline = "1px solid red";
+      let errorSpan = document.createElement("span");
+      errorSpan.textContent = errorMessage;
+      errorSpan.id = errorMessageId;
+      errorSpan.className = "error-message";
+      inputElement.parentNode.after(errorSpan);
+    }
+  }
 
   if (status === "edit") {
     return (
       <div className="general-input">
         <h1>General Information: </h1>
         <form action="">
-          <p>* Designates an optional field</p>
+          <p>
+            <span className="required-asterisk">*</span> Designates a required
+            field
+          </p>
           <label id="name-input-label">
-            Name:
+            Name<span className="required-asterisk">*</span>:
             <input
               type="text"
               value={person.name}
               onChange={(event) =>
-                setPerson({
-                  ...person,
-                  name: event.target.value,
-                })
+                validityChecker(
+                  event,
+                  document.querySelector("#name-input-label input"),
+                  "name",
+                  "Field requires text",
+                  "general-name-input-error",
+                )
               }
               placeholder="ex: Chris Toms"
+              required
             />
           </label>
           <label id="email-input-label">
-            Email:
+            Email<span className="required-asterisk">*</span>:
             <input
-              type="text"
+              type="email"
               value={person.email}
               onChange={(event) =>
-                setPerson({ ...person, email: event.target.value })
+                validityChecker(
+                  event,
+                  document.querySelector("#email-input-label input"),
+                  "email",
+                  "Field requires valid email address",
+                  "general-email-input-error",
+                )
               }
               placeholder="ex: chrisT@test.com"
+              required
+              pattern="\w+@{1}[a-zA-Z]+[.][a-zA-Z]+"
             />
           </label>
           <label id="phone-input-label">
-            Phone:
+            Phone<span className="required-asterisk">*</span>:
             <input
               type="text"
               value={person.phone}
               onChange={(event) =>
-                setPerson({ ...person, phone: event.target.value })
+                validityChecker(
+                  event,
+                  document.querySelector("#phone-input-label input"),
+                  "phone",
+                  "Field requires 10 digits",
+                  "general-phone-input-error",
+                )
               }
               placeholder="ex: 1234567890"
+              required
+              pattern="\d{10}"
             />
           </label>
           <label id="location-input-label">
-            Location*:
+            Location<span className="required-asterisk">*</span>:
             <input
               type="text"
               value={person.location}
               onChange={(event) =>
-                setPerson({ ...person, location: event.target.value })
+                validityChecker(
+                  event,
+                  document.querySelector("#location-input-label input"),
+                  "location",
+                  "Field requires text entry",
+                  "general-location-input-error",
+                )
               }
               placeholder="city, state"
+              required
             />
           </label>
           <label id="professional-title-input-label">
-            Professional Title*:
+            Professional Title:
             <input
               type="text"
               value={person.title}
@@ -69,7 +127,7 @@ export default function General({ person, setPerson }) {
             />
           </label>
           <label id="description-input-label">
-            Description*:
+            Description:
             <textarea
               type="text"
               value={person.description}
@@ -80,7 +138,7 @@ export default function General({ person, setPerson }) {
             />
           </label>
           <label id="skills-input-label">
-            Skills*:
+            Skills:
             <input
               type="text"
               id="skills-input"
@@ -88,15 +146,40 @@ export default function General({ person, setPerson }) {
               onChange={(event) =>
                 setPerson({ ...person, skills: event.target.value.split(", ") })
               }
+              placeholder="Comma space entries; ex: a, b, c"
             />
-            <span className="skills-disclaimer">
-              Please comma space separate entries (ex: strong communication,
-              critical thinking, teamwork, adaptability)
-            </span>
           </label>
           <button
             id="submit-general"
-            onClick={() => setStatus("submit")}
+            onClick={() => {
+              //if submit button error message shown then delete it
+              if (document.querySelector("#submit-button-error")) {
+                document.querySelector("#submit-button-error").remove();
+              }
+
+              //apply outline color if required input fields are invalid
+              let invalidInputs = document.querySelectorAll(
+                "input[required]:invalid",
+              );
+              invalidInputs.forEach(
+                (input) => (input.style.outline = "1px solid red"),
+              );
+
+              //checks if any required inputs are invalid/blank and displays error message if so
+              if (invalidInputs.length === 0) {
+                setStatus("submit");
+              } else {
+                //add error message below span, but somehow limit the number of them, and take them away when the button is pressed again
+                let submitErrorMessageSpan = document.createElement("span");
+                submitErrorMessageSpan.className = "error-message";
+                submitErrorMessageSpan.id = "submit-button-error";
+                submitErrorMessageSpan.textContent =
+                  "There are invalid or missing input in the fields above, please input entry into fields, and follow any error prompts  that follow your entry";
+                document
+                  .querySelector("#submit-general")
+                  .after(submitErrorMessageSpan);
+              }
+            }}
             type="button"
           >
             Submit

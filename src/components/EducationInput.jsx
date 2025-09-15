5b79efc55809d6a4ef135aae0ed72e304687d788
Andrew Perotti <Perotti28@gmail.com>
2025-09-15 12:55:06 -0700

    feat: add asterisk explainer/disclaimer
    This tells users that an input field wrapped by a label with an asterisk in its text content denotes that field is required to have input.
    
    feat: add asterisks for content of required inputs
    
    This obviously tells the user that this input is required.
    
    feat: add form
    
    Before the input elements were just existing within the component instances, even though we don't use submit for anything because our buttons are buttons and not submit type I've gone ahead and wrapped the labels and inputs with the form.
    
    feat: add delete button
    
    This button allows users to delete EducationInput components, therefore removing not only the educations they've added but potentially the default education object as well, which is ok because we have dynamic rendering in the submitted form of our application depending on what they submit it will look presentable.
    
    fix: change date descriptors startDate and endDate
    
    These names we're changing to already existed in the education objects of person by default so I'll go ahead and use them instead.

--- src/components/EducationInput.jsx
+++ src/components/EducationInput.jsx
@@ -2,7 +2,6 @@ import "../styles/EducationInput.css";
 
 function EducationInput({ person, setPerson, index }) {
   function addOrChangeEducation(property, value) {
-    //add/replace property: value pair, as long as there is an object at that index then you can add or change the property value pair
     let newPerson = { ...person };
     newPerson.education[index][property] = value;
     setPerson(newPerson);
@@ -10,49 +9,78 @@ function EducationInput({ person, setPerson, index }) {
 
   return (
     <div className="education-input-instance">
-      <label className="institution-input-label">
-        Institution/Course Name:
-        <input
-          value={person.education[index].institutionName}
-          onChange={(event) =>
-            addOrChangeEducation("institutionName", event.target.value)
-          }
-        />
-      </label>
+      <form action="">
+        <p>
+          <span className="required-asterisk">*</span> Designates a required
+          field
+        </p>
+        <label className="institution-input-label">
+          <span>
+            Institution/Course Name<span className="required-asterisk">*</span>:
+          </span>
+          <input
+            value={person.education[index].institutionName}
+            required
+            onChange={(event) =>
+              addOrChangeEducation("institutionName", event.target.value)
+            }
+          />
+        </label>
 
-      <label className="degree-input-label">
-        Title of degree/course:
-        <input
-          value={person.education[index].courseName}
-          onChange={(event) =>
-            addOrChangeEducation("courseTitle", event.target.value)
-          }
-          type="string"
-        />
-      </label>
+        <label className="degree-input-label">
+          <span>
+            Title of degree/course<span className="required-asterisk">*</span>:
+          </span>
+          <input
+            value={person.education[index].courseTitle}
+            required
+            onChange={(event) =>
+              addOrChangeEducation("courseTitle", event.target.value)
+            }
+            type="string"
+          />
+        </label>
 
-      <label className="start-date-input-label">
-        Date Attended/Studied Start:
-        <input
-          type="date"
-          value={person.education[index].dateStarted}
-          onChange={(event) =>
-            addOrChangeEducation("startDate", event.target.value)
-          }
-        />
-      </label>
+        <label className="start-date-input-label">
+          <span>
+            Date Attended/Studied Start
+            <span className="required-asterisk">*</span>:
+          </span>
+          <input
+            type="date"
+            required
+            value={person.education[index].startDate}
+            onChange={(event) =>
+              addOrChangeEducation("startDate", event.target.value)
+            }
+          />
+        </label>
 
-      <label className="end-date-input-label">
-        Date Attended/Studied End:
-        <input
-          type="date"
-          value={person.education[index].dateEnded}
-          onChange={(event) =>
-            addOrChangeEducation("endDate", event.target.value)
-          }
-        />
-      </label>
-      <hr></hr>
+        <label className="end-date-input-label">
+          Date Attended/Studied End: (If attending leave blank)
+          <input
+            type="date"
+            value={person.education[index].endDate}
+            onChange={(event) =>
+              addOrChangeEducation("endDate", event.target.value)
+            }
+          />
+        </label>
+
+        <button
+          className="delete-education-button"
+          type="button"
+          onClick={() => {
+            let newPerson = { ...person };
+            newPerson.education = newPerson.education.filter(
+              (education, eduIndex) => eduIndex !== index,
+            );
+            setPerson({ ...newPerson });
+          }}
+        >
+          Delete
+        </button>
+      </form>
     </div>
   );
 }

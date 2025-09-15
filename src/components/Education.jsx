740f63fa52ae4b92ff7dfb65005340a1d4d69492
Andrew Perotti <Perotti28@gmail.com>
2025-09-15 11:42:15 -0700

    docs: remove or rewrite comments
    These are for explaining what I'm doing if complex, and contain notes for code I need to write in the future
    
    fix: change argument to key & index EducationInput
    
    These arguments are index values of the new sliced array, but we need index of the elements in the original array, so we'll increment these arguments by one because we're using a slice of the original array minus the first education object that gets rendered by default.
    
    feat: add a limit of ten EducationInput objects
    
    Otherwise a user could just keep adding more objects. There is right now a problem, if a user adds more than they need or want, there is no way of getting rid of them. So I need to add a delete button for that. In addition to the limit I've added an error message that pops up if a user reaches that limit of EducationInput components.
    
    feat: add validity checks for EducationInput
    
    These validity checks on input elements disallow the user to submit incomplete education data to the submit result which would create empty ugly list items.. By adding a future delete button we'll give users the ability to remove falsely made education objects that make their CV look ugly. Applied styling for when the required education inputs entry is not valid, and later will add messages under each input to describe to users the problems of their input.
    
    fix: remove some education property checks
    
    When a user submits there is a condition that checks if there are education objects in person, before I added required to input elements this condition also checked for an institutionName length greater than one before it rendered which isn't needed any longer because we have added validation checks and requirements to input fields including institutionName.
    
    fix: change how dates are rendered
    
    Before this change if a starting or ending date value was empty then it would render empty before or after /'s in the format of the output when rendering submitted data. Now startDate is a required field and endDate is optional and when not entered 'present' will be rendered instead of an empty string.

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -6,13 +6,12 @@ export default function Education({ person, setPerson }) {
   const [status, setStatus] = useState("edit");
 
   if (status === "edit") {
-    //if person.education.length===0 render one instance with no values
-    //else if person.education.length>0 render an EducationInput instance for each object in that array
     return (
+      //generate first education object, there's one by default
+      //if there are more education objects generate a component for each
+
       <div id="education-input">
         <h1>Education:</h1>
-
-        {/* render first education element, which exists by default*/}
         {
           <EducationInput
             person={person}
@@ -21,43 +20,80 @@ export default function Education({ person, setPerson }) {
             index={0}
           />
         }
-
-        {/* render the rest of the education objects if the length is greater than 1, which happens when the "add another education" button is clicked and updates state*/}
         {person.education.length > 1 &&
-          person.education
-            .slice(1)
-            .map((element, index) => (
+          person.education.slice(1).map((element, index) => (
+            <>
+              <hr></hr>
               <EducationInput
                 person={person}
                 setPerson={setPerson}
-                key={index}
-                index={index}
+                key={index + 1}
+                index={index + 1}
               />
-            ))}
+            </>
+          ))}
 
         <button
           id="add-education-button"
+          //Limit new educations to ten
           onClick={() => {
-            let newPerson = { ...person };
-            newPerson.education.push({
-              institutionName: "",
-              courseTitle: "",
-              startDate: "",
-              endDate: "",
-            });
-            setPerson(newPerson);
+            if (person.education.length < 10) {
+              let newPerson = { ...person };
+              newPerson.education.push({
+                institutionName: "",
+                courseTitle: "",
+                startDate: "",
+                endDate: "",
+              });
+              setPerson(newPerson);
+            } else {
+              //tell the user that there are only ten educations allowed
+              let educationLimitError = document.createElement("span");
+              educationLimitError.id = "education-limit-error-message";
+              educationLimitError.textContent =
+                "You cannot have more than ten education objects";
+              document
+                .querySelector("#add-education-button")
+                .after(educationLimitError);
+              setTimeout(() => {
+                educationLimitError.remove();
+              }, 5000);
+            }
           }}
         >
-          Add Another
+          Add Another Education
         </button>
-        <button onClick={() => setStatus("submit")}>Submit</button>
+
+        <button
+          onClick={() => {
+            //if required input fields data are acceptable, then change status
+            let educationInputFields = Array.from(
+              document.querySelectorAll(".education-input-instance input"),
+            );
+            if (
+              educationInputFields.every(
+                (input) => input.checkValidity() === true,
+              )
+            ) {
+              setStatus("submit");
+            } else {
+              //otherwise tell the user there are problems with their input above
+              educationInputFields.filter((inputEle) =>
+                inputEle.checkValidity() === false
+                  ? (inputEle.style.outline = "1px solid red")
+                  : null,
+              );
+            }
+          }}
+        >
+          Submit
+        </button>
       </div>
     );
   } else {
     return (
       <div id="education-output">
-        {person.education.length > 0 &&
-          person.education[0].institutionName.length > 0 && <h1>Education</h1>}
+        {person.education.length > 0 && <h1>Education</h1>}
         <ul className="education-list">
           {person.education.map((education, index) => (
             <li className="education-items" key={index}>
@@ -69,8 +105,10 @@ export default function Education({ person, setPerson }) {
                   {education.startDate.slice(5, 7)}/
                   {education.startDate.slice(9)}/
                   {education.startDate.slice(0, 4)} to{" "}
-                  {education.endDate.slice(5, 7)}/{education.endDate.slice(9)}/
-                  {education.endDate.slice(0, 4)}
+                  {education.endDate
+                    ? `${education.endDate.slice(5, 7)}/${education.endDate.slice(9)}/
+                  ${education.endDate.slice(0, 4)}`
+                    : "Present"}
                 </span>
               </section>
               <p className="education-title">{education.courseTitle}</p>

f4a2ed520dd93d1cb74c30d95550addfbe6eb68d
Andrew Perotti <Perotti28@gmail.com>
2025-09-16 10:27:02 -0700

    feat: add functions file
    This file stores the functions used by components across the application.
    changeArrayObjectsValue is used to save state of the Person when needing to change a value of a property of an element in an array of a category of Person, like education.title. This will be used in the professional section as well
    validityChecker is very important as it's used in all of the input components to check if user data input is valid for each input field that is required.
    saveInput contains the most basic property value change functionality but is still cleaner to call than the three lines it contains.
    verifyInputs is invoked when the submit button of a component is clicked and checks if the inputs of that component type are valid, if not applies styling and an error message below the submit button.

---
+++ src/assets/functions.js
@@ -0,0 +1,66 @@
+function changeArrayObjectValue(
+  category,
+  property,
+  value,
+  person,
+  setPerson,
+  index,
+) {
+  let newPerson = { ...person };
+  newPerson[category][index][property] = value;
+  setPerson(newPerson);
+}
+
+function validityChecker(inputElement, errorMessage, errorMessageId) {
+  //check if specific error message is active, if so delete
+  if (document.querySelector(`#${errorMessageId}`)) {
+    document.querySelector(`#${errorMessageId}`).remove();
+  }
+  //check if input is valid, remove styling
+  if (inputElement.checkValidity() === true) {
+    inputElement.style.outline = "none";
+  } else {
+    inputElement.style.outline = "1px solid red";
+    let errorSpan = document.createElement("span");
+    errorSpan.textContent = errorMessage;
+    errorSpan.id = errorMessageId;
+    errorSpan.className = "error-message";
+    inputElement.parentNode.after(errorSpan);
+  }
+}
+
+function saveInput(event, property, person, setPerson) {
+  let newPerson = { ...person };
+  newPerson[property] = event.target.value;
+  setPerson({ ...newPerson });
+}
+
+function verifyInputs(section, setStatus) {
+  //if submit button error message shown then delete it
+  if (document.querySelector(`#${section}-submit-button-error`)) {
+    document.querySelector(`#${section}-submit-button-error`).remove();
+  }
+
+  //apply outline color if required input fields are invalid
+  let invalidInputs = document.querySelectorAll(
+    `#${section}-input input[required]:invalid`,
+  );
+  invalidInputs.forEach((input) => (input.style.outline = "1px solid red"));
+
+  //checks if any required inputs are invalid/blank and displays error message if so
+  if (invalidInputs.length === 0) {
+    setStatus("submit");
+  } else {
+    //add error message below span, but somehow limit the number of them, and take them away when the button is pressed again
+    let submitErrorMessageSpan = document.createElement("span");
+    submitErrorMessageSpan.className = "error-message";
+    submitErrorMessageSpan.id = `${section}-submit-button-error`;
+    submitErrorMessageSpan.textContent =
+      "There are invalid or missing input in the fields above, please input entry into fields, and follow any error prompts  that follow your entry";
+    document
+      .querySelector(`#submit-${section}-button`)
+      .after(submitErrorMessageSpan);
+  }
+}
+
+export { changeArrayObjectValue, validityChecker, saveInput, verifyInputs };

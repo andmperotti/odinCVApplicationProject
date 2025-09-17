68e6f3f4f3c87929db577aad750d5988eeb357a5
Andrew Perotti <Perotti28@gmail.com>
2025-09-17 14:28:36 -0700

    feat: add function for component creation
    feat: give component person and index prop imports
    
    feat: have the component render saved data
    
    This component pulls data from the person state variable, specifically it's professional property which is an array of objects and generates elements for each piece of that data of the object. Some data requires output to be dynamic, for example if a professional object has a start date but not an end date then rather than display an empty string it'll show "Present" as it's presumed that the user is still working at that job. Another example would be responsibilities are generated into a list iteratively.

--- src/components/ProfessionalOutput.jsx
+++ src/components/ProfessionalOutput.jsx
@@ -1,1 +1,32 @@
 import "../styles/ProfessionalOutput.css";
+
+export function ProfessionalOutput({ person, index }) {
+  return (
+    <section id={`professional-output-${index}`}>
+      <h3>{person.professional[index].companyName}</h3>
+      <span>
+        {person.professional[index].startDate.split("-")[1]}/
+        {person.professional[index].startDate.split("-")[2]}/
+        {person.professional[index].startDate.split("-")[0]}
+        to{" "}
+        {person.professional[index].endDate
+          ? `${person.professional[index].endDate.split("-")[1]}/${person.professional[index].endDate.split("-")[2]}/${person.professional[index].endDate.split("-")[0]}`
+          : "Present"}
+      </span>
+      <p>{person.professional[index].positionHeld}</p>
+      <ul>
+        {person.professional[index].responsibilities.length > 0 &&
+          person.professional[index].responsibilities.map(
+            (responsibility, responsibilityIndex) => (
+              <li
+                key={index}
+                id={`professional-output-${index}-responsibility-${responsibilityIndex}`}
+              >
+                {responsibility}
+              </li>
+            ),
+          )}
+      </ul>
+    </section>
+  );
+}

2b8b880ed68e9eb3275953b1d7324fddb6546e34
Andrew Perotti <Perotti28@gmail.com>
2025-09-17 11:57:13 -0700

    build: add import and export statement/s

---
+++ src/components/ProfessionalInput.jsx
@@ -0,0 +1,9 @@
+import "../styles/ProfessionalInput.css";
+import {
+  changeArrayObjectValue,
+  validityChecker,
+} from "../assets/functions.js";
+
+function ProfessionalInput({ person, setPerson, index }) {}
+
+export { ProfessionalInput };

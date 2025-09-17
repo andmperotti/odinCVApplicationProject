3b08c1ac8c12624b2cb7ac85f5d8fa056c9deac6
Andrew Perotti <Perotti28@gmail.com>
2025-09-17 12:29:00 -0700

    docs: add import and export statements
    Imports include methods from a project module that we'll use to validation and verify inputs and to save input to our state variable of person. And of course exporting this component so it can be used in the App Component which is the root component of our application.
    
    feat: create a variable with useState
    
    This variable will track if our component is in 'edit' or 'submit' mode/value, and that value will be used conditionally to render input components or output of data of person.education objects.
    
    feat: add conditions to render dynamic content
    
    These conditions check the number of person.professional objects, and depending on that the user will see input fields or a statement alerting them that there are no professional objects in person and in either case gives the user two buttons, one to add professional objects to person, and the other to render their professional objects into output or to hide the section in the output / 'submit' state so that there isn't an empty Professional section rendered on their CV.
    
    feat: add starting component invocations
    
    These don't include props yet, they are just boilerplate using the child components name.

--- src/components/Professional.jsx
+++ src/components/Professional.jsx
@@ -1,10 +1,41 @@
-// import { useState } from "react";
-import "../styles/index.css";
+import { useState } from "react";
+import "../styles/Professional.css";
+import { verifyInputs } from "../assets/functions";
+import { ProfessionalInput } from "./ProfessionalInput";
 
-export default function Professional() {
-  return (
-    <>
-      <h1>Professional Experience:</h1>
-    </>
-  );
+export default function Professional({ person, setPerson }) {
+  const [status, setStatus] = useState("edit");
+
+  if (status === "edit") {
+    if (person.professional.length > 0) {
+      return (
+        <>
+          <h1>Professional Experience:</h1>
+          {/* //render only one instance of ProfessionalInput */}
+          {person.professional.length === 1 && (
+            <div key={0}>
+              <ProfessionalInput person={person} setPerson={setPerson} />
+            </div>
+          )}
+          {/* //render a ProfessionalInput component for each person.professional object */}
+          {person.professional.length > 1 &&
+            person.professional.map((profession, index) => (
+              <div key={index}>
+                <ProfessionaInput person={person} setPerson={setPerson} />
+              </div>
+            ))}
+          {/* no professional experience/objects */}
+          {person.professional.length === 0 && (
+            <p>No professional experience objects exist!</p>
+          )}
+          {/* //add button //submit button} */}
+          <button type="button">Add Another Profession</button>
+          <button type="button">Submit</button>
+        </>
+      );
+    }
+  } else {
+    //submit mode
+    
+  }
 }

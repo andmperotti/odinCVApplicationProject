9fe45e8114e2c583830498ede0c6fe5fdad0615d
Andrew Perotti <Perotti28@gmail.com>
2025-09-18 16:48:32 -0700

    feat: add addProfession import
    This function will allow the user to create more professional experience objects so they can add more jobs they've worked in the past. By putting this in a module I'm saving the clutter to a more appropriate place.
    
    refactor: rewrite render when status === 'edit'
    
    Upon testing I noticed that the elements being rendered weren't quite what I needed and that would be fixed by refactoring the entire return.
    
    feat: add logic for add professional experience
    
    This button had no code tied to it and was not attempting anything when clicked, now creates another object in person.professional array.
    
    fix: remove setPerson invocation from submit
    
    This was an accidental addition, verifyInputs calls setPerson, which is bad because that function is doing too much but anyways.
    
    refactor: rewrite render when status === 'submit'
    
    Just like the 'edit' version, the output was wrong and I needed to fix it, actually this one was worse, there were two "Experience" headings that were rendering and that's what alerted me to needing to rewrite the section.

--- src/components/Professional.jsx
+++ src/components/Professional.jsx
@@ -1,45 +1,76 @@
 import { useState } from "react";
 import "../styles/Professional.css";
-import { verifyInputs } from "../assets/functions";
+import { verifyInputs, addProfession } from "../assets/functions";
 import { ProfessionalInput } from "./ProfessionalInput";
+import { ProfessionalOutput } from "./ProfessionalOutput";
 
 export default function Professional({ person, setPerson }) {
   const [status, setStatus] = useState("edit");
 
   if (status === "edit") {
-    if (person.professional.length > 0) {
-      return (
-        <>
-          <h1>Professional Experience:</h1>
-          {/* //render only one instance of ProfessionalInput */}
-          {person.professional.length === 1 && (
-            <div key={0}>
-              <ProfessionalInput person={person} setPerson={setPerson} />
+    return (
+      <div id="professional-input">
+        <h1>Professional Experience:</h1>
+        {/* //render a ProfessionalInput component for each person.professional object */}
+        {person.professional.length > 0 &&
+          person.professional.map((profession, index) => (
+            <div key={index} className="professional-input">
+              <ProfessionalInput
+                person={person}
+                setPerson={setPerson}
+                index={index}
+              />
+              <hr className="professional-instance-hr"></hr>
             </div>
-          )}
-          {/* //render a ProfessionalInput component for each person.professional object */}
-          {person.professional.length > 1 &&
-            person.professional.map((profession, index) => (
-              <div key={index}>
-                <ProfessionaInput person={person} setPerson={setPerson} />
-              </div>
-            ))}
-          {/* no professional experience/objects */}
-          {person.professional.length === 0 && (
-            <p>No professional experience objects exist!</p>
-          )}
-          {/* //add button //submit button} */}
-          <button type="button">Add Another Profession</button>
-          <button type="button">Submit</button>
-        </>
-      );
-    }
+          ))}
+        {/* no professional experience/objects */}
+        {person.professional.length === 0 && (
+          <p>No professional experience objects exist!</p>
+        )}
+        {/* //add button //submit button} */}
+        <button type="button" onClick={() => addProfession(person, setPerson)}>
+          Add Another Professional Experience
+        </button>
+        <button
+          id="submit-professional-button"
+          type="button"
+          onClick={() => {
+            verifyInputs("professional", setStatus);
+          }}
+        >
+          Submit
+        </button>
+      </div>
+    );
   } else {
     //submit mode
-    // if person has profession objects, render their values
-    {
-      person.professional.length > 0 && <h2>Professional Experience:</h2>;
-    }
-    // {person.professional.length > 0 && person.professional.map((profession, index)=>
+    return (
+      <section id="professional-output">
+        {person.professional.length === 0 && (
+          <h1 className="no-profession">Experience</h1>
+        )}
+        {person.professional.length > 0 && (
+          <h1 className="has-profession">Experience</h1>
+        )}
+        {/* // if person has professional objects, render their values */}
+        {person.professional.length > 0 &&
+          person.professional.map((profession, index) => (
+            <ProfessionalOutput person={person} index={index} key={index} />
+          ))}
+        {/* if person has no professional objects, remind them  */}
+        {person.professional.length === 0 && (
+          <p className="no-profession">
+            No professional experience created so far
+          </p>
+        )}
+        <button
+          className="edit-button"
+          id="professional-edit-button"
+          onClick={() => setStatus("edit")}
+        >
+          Edit
+        </button>
+      </section>
+    );
   }
 }

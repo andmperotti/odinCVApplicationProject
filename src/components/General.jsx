011c3ffa5d8255ae6bd54da093e881e76363ac1b
Andrew Perotti <Perotti28@gmail.com>
2025-09-19 12:52:21 -0700

    refactor: move General input renderings
    This logic has all been moved to GeneralInput, including the importing of the functions used in that form of inputs to validate and generate error messages to users. The setStatus state variable function has been passed as a prop to it so it can change 'status' just as it did before.
    Next I'll create a skills component to replace the input element that asks users to enter values comma space separated as that is terrible to ask users to do.

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,6 +1,6 @@
 import { Children, useState } from "react";
 import "../styles/General.css";
-import { saveInput, validityChecker, verifyInputs } from "../assets/functions";
+import { GeneralInput } from "./GeneralInput";
 
 export default function General({ person, setPerson }) {
   const [status, setStatus] = useState("edit");
@@ -9,134 +9,12 @@ export default function General({ person, setPerson }) {
     return (
       <div id="general-input">
         <h1>General Information: </h1>
-        <form action="">
-          <p>
-            <span className="required-asterisk">*</span> Designates a required
-            field
-          </p>
-          <label id="name-input-label">
-            <span>
-              Name<span className="required-asterisk">*</span>:
-            </span>
-            <input
-              type="text"
-              value={person.name}
-              onChange={(event) => {
-                saveInput(event, "name", person, setPerson);
-                validityChecker(
-                  document.querySelector("#name-input-label input"),
-                  "Field requires text",
-                  "general-name-input-error",
-                );
-              }}
-              placeholder="ex: Chris Toms"
-              required
-            />
-          </label>
-          <label id="email-input-label">
-            <span>
-              Email<span className="required-asterisk">*</span>:
-            </span>
-            <input
-              type="email"
-              value={person.email}
-              onChange={(event) => {
-                saveInput(event, "email", person, setPerson);
-                validityChecker(
-                  document.querySelector("#email-input-label input"),
-                  "Field requires valid email address",
-                  "general-email-input-error",
-                );
-              }}
-              placeholder="ex: chrisT@test.com"
-              required
-              pattern="\w+@{1}[a-zA-Z]+[.][a-zA-Z]+"
-            />
-          </label>
-          <label id="phone-input-label">
-            <span>
-              Phone<span className="required-asterisk">*</span>:
-            </span>
-            <input
-              type="text"
-              value={person.phone}
-              onChange={(event) => {
-                saveInput(event, "phone", person, setPerson);
-                validityChecker(
-                  document.querySelector("#phone-input-label input"),
-                  "Field requires 10 digits",
-                  "general-phone-input-error",
-                );
-              }}
-              placeholder="ex: 1234567890"
-              required
-              pattern="\d{10}"
-            />
-          </label>
-          <label id="location-input-label">
-            <span>
-              Location<span className="required-asterisk">*</span>:
-            </span>
-            <input
-              type="text"
-              value={person.location}
-              onChange={(event) => {
-                saveInput(event, "location", person, setPerson);
-                validityChecker(
-                  document.querySelector("#location-input-label input"),
-                  "Field requires text entry",
-                  "general-location-input-error",
-                );
-              }}
-              placeholder="city, state"
-              required
-            />
-          </label>
-          <label id="professional-title-input-label">
-            Professional Title:
-            <input
-              type="text"
-              value={person.title}
-              onChange={(event) =>
-                setPerson({ ...person, title: event.target.value })
-              }
-              placeholder="ex: Full Stack Developer"
-            />
-          </label>
-          <label id="description-input-label">
-            Description:
-            <textarea
-              type="text"
-              value={person.description}
-              onChange={(event) =>
-                setPerson({ ...person, description: event.target.value })
-              }
-              placeholder="3-5 sentence summary that highlights your most relevant skills, experience, and career goals for the specific job"
-            />
-          </label>
-          <label id="skills-input-label">
-            Skills:
-            <input
-              type="text"
-              id="skills-input"
-              value={person.skills.join(", ")}
-              onChange={(event) =>
-                setPerson({ ...person, skills: event.target.value.split(", ") })
-              }
-              placeholder="Comma space entries; ex: a, b, c"
-            />
-          </label>
-
-          {/* button to add SkillInput instance */}
-
-          <button
-            id="submit-general-button"
-            onClick={() => verifyInputs("general", setStatus)}
-            type="button"
-          >
-            Submit
-          </button>
-        </form>
+        <GeneralInput
+          person={person}
+          setPerson={setPerson}
+          status={status}
+          setStatus={setStatus}
+        />
       </div>
     );
   } else {

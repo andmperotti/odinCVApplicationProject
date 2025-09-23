28cbd53ac868d1afdb57b1144e4f50622ae4133a
Andrew Perotti <Perotti28@gmail.com>
2025-09-23 10:24:31 -0700

    feat: add hr to separate general from education
    This will help the user to understand a little more that when they click the submit general button that it only submits entry in that form and nothing below this separator.
    
    refactor: move output to GeneralOutput component
    
    For organization efforts I've made a GeneralOutput component and passed it person and setStatus so it can handle everything that was generated in the else status==='submit' condition's return body.

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,6 +1,7 @@
-import { Children, useState } from "react";
+import { useState } from "react";
 import "../styles/General.css";
 import { GeneralInput } from "./GeneralInput";
+import { GeneralOutput } from "./GeneralOutput";
 
 export default function General({ person, setPerson }) {
   const [status, setStatus] = useState("edit");
@@ -15,64 +16,13 @@ export default function General({ person, setPerson }) {
           status={status}
           setStatus={setStatus}
         />
+        <hr className="section-divide"></hr>
       </div>
     );
   } else {
     return (
       <div id="general-output">
-        {person.name.length > 0 && (
-          <h1 className="name-output">{person.name}</h1>
-        )}
-        {person.title.length > 0 && (
-          <h3 className="title-output">{person.title}</h3>
-        )}
-        {(person.email.length > 0 || person.phone.length > 0) && (
-          <div className="phone-email-output">
-            {person.phone.length > 0 && (
-              <span>
-                <span className="phone-keyword">Phone:</span> (
-                {person.phone.slice(0, 3)}){person.phone.slice(3, 6)}-
-                {person.phone.slice(6)}
-              </span>
-            )}
-            {person.phone.length > 0 && person.email.length > 0 && (
-              <span> </span>
-            )}
-            {person.email.length > 0 && (
-              <span>
-                <span className="email-keyword">Email:</span> {person.email}
-              </span>
-            )}
-          </div>
-        )}
-        {person.location.length > 0 && (
-          <p className="person-location-output">{person.location}</p>
-        )}
-        {person.description.length > 0 ? (
-          <>
-            <h2 className="description-output-title">Description: </h2>
-            <p>{person.description}</p>
-          </>
-        ) : null}
-        {person.skills.length > 0 ? (
-          <>
-            <h2 className="skills-output-title">Skills:</h2>
-            <ul className="skills-output-ul">
-              {person.skills.map((skill, index) => (
-                <li key={index} className="skill-li">
-                  {skill}
-                </li>
-              ))}
-            </ul>
-          </>
-        ) : null}
-        <button
-          onClick={() => setStatus("edit")}
-          type="button"
-          className="edit-button"
-        >
-          Edit
-        </button>
+        <GeneralOutput person={person} setStatus={setStatus} />
       </div>
     );
   }
